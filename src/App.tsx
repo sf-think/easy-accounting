ec70eee5fc275849f668bb2eb7e53a3561304f33
murphy <murphy.zh@outlook.com>
2023-10-10 23:59:31 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { defineComponent, ref } from 'vue'
+
+export const App = defineComponent({
+  setup() {
+    const count = ref(0)
+    return () => (
+      <>
+        <div>{count.value}</div>
+        <button onClick={() => count.value++}> +1 </button>
+      </>
+    )
+  }
+})
